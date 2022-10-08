06bc6a7112f8beb6d5916b3c6771722a29436c59
com <xworkzodc@gmail.com>
2022-10-08 08:47:47 +0530

    mvc

--- songsandfood/src/main/java/com/xworkz/songsandfood/dto/FoodDTO.java
+++ songsandfood/src/main/java/com/xworkz/songsandfood/dto/FoodDTO.java
@@ -2,16 +2,6 @@ package com.xworkz.songsandfood.dto;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class FoodDTO implements Serializable {
 	private String itemName;
 	private Double price;
@@ -20,4 +10,53 @@ public class FoodDTO implements Serializable {
 	private String location;
 	private String ordered;
 	private Integer quantity;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getOrdered() {
+		return ordered;
+	}
+	public void setOrdered(String ordered) {
+		this.ordered = ordered;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "FoodDTO [itemName=" + itemName + ", price=" + price + ", hotelName=" + hotelName + ", type=" + type
+				+ ", location=" + location + ", ordered=" + ordered + ", quantity=" + quantity + "]";
+	}
+	
+	
 }
--- songsandfood/src/main/java/com/xworkz/songsandfood/dto/SongDTO.java
+++ songsandfood/src/main/java/com/xworkz/songsandfood/dto/SongDTO.java
@@ -2,16 +2,6 @@ package com.xworkz.songsandfood.dto;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class SongDTO implements Serializable{
 	private String name;
 	private String singer;
@@ -20,5 +10,52 @@ public class SongDTO implements Serializable{
 	private String lyrics;
 	private String language;
 	private String type;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getProducer() {
+		return producer;
+	}
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+	public String getLyrics() {
+		return lyrics;
+	}
+	public void setLyrics(String lyrics) {
+		this.lyrics = lyrics;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "SongDTO [name=" + name + ", singer=" + singer + ", duration=" + duration + ", producer=" + producer
+				+ ", lyrics=" + lyrics + ", language=" + language + ", type=" + type + "]";
+	}
 
 }
